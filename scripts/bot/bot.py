aa4628d18a7c1514031ce90ca4c8fab3bfa9dbbe
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2024-05-16 14:58:31 +0500

    Delete scripts directory

--- scripts/bot/bot.py
+++
@@ -1,5 +0,0 @@
-import telebot
-from scripts.config.config import tok
-
-# основная переменная с функционалом telebot API
-bot = telebot.TeleBot(tok)
--- scripts/message/message.py
+++
@@ -1,4 +0,0 @@
-# возвращает сообщение в нижнем регистре
-def get_message_lower(message:str):
-    message = message.text.lower()
-    return message
